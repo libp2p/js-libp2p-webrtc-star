0aa77611cf91f24b8d6918930abd87e519cb09fc
David Dias <daviddias.p@gmail.com>
2016-08-19 15:07:44 +0100

    feat(pull-api): update the tests accordingly

--- test/webrtc-star/test-valid-connection.js
+++ test/webrtc-star/test-valid-connection.js
@@ -4,10 +4,11 @@
 const expect = require('chai').expect
 const multiaddr = require('multiaddr')
 const series = require('run-series')
+const pull = require('pull-stream')
 
 const WebRTCStar = require('../../src/webrtc-star')
 
-describe.skip('valid Connection', () => {
+describe('valid Connection', () => {
   let ws1
   const ma1 = multiaddr('/libp2p-webrtc-star/ip4/127.0.0.1/tcp/15555/ws/ipfs/QmcgpsyWgH8Y8ajJz1Cu72KnS5uo2Aa2LpzU7kinSooo5a')
 
@@ -26,7 +27,7 @@ describe.skip('valid Connection', () => {
       ws1 = new WebRTCStar()
 
       const listener = ws1.createListener((conn) => {
-        conn.pipe(conn)
+        pull(conn, conn)
       })
 
       listener.listen(ma1, next)
@@ -36,14 +37,13 @@ describe.skip('valid Connection', () => {
       ws2 = new WebRTCStar()
 
       const listener = ws2.createListener((conn) => {
-        conn.pipe(conn)
+        pull(conn, conn)
       })
       listener.listen(ma2, next)
     }
 
     function dial () {
-      conn = ws1.dial(ma2)
-      conn.on('connect', done)
+      conn = ws1.dial(ma2, done)
     }
   })
 
