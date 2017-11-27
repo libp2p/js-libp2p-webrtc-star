171dc9bcc35551dba9624efcf7c18b479768ccc2
Maciej Krüger <mkg20001@gmail.com>
2017-11-27 13:58:48 +0100

    feat: Add metrics and about page (#127)
    * feat: Metrics and about page
    
    * fix: Rename metrics
    
    * feat: Add metrics to cli

--- .aegir.js
+++ .aegir.js
@@ -1,13 +1,17 @@
 'use strict'
 
 const sigServer = require('./src/sig-server')
+let firstRun = true
 let sigS
 
 function boot (done) {
   const options = {
     port: 15555,
-    host: '127.0.0.1'
+    host: '127.0.0.1',
+    metrics: firstRun
   }
+
+  if (firstRun) { firstRun = false }
 
   sigServer.start(options, (err, server) => {
     if (err) { throw err }
