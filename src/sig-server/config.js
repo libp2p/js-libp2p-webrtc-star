a9737c2e36df64daa2e5fc34105a39a2f44cccca
Dmitriy Ryajov <dryajov@gmail.com>
2018-12-13 20:12:53 -0600

    feat: only send new peers, not the whole peer list

--- src/sig-server/config.js
+++ src/sig-server/config.js
@@ -4,7 +4,7 @@ const debug = require('debug')
 const log = debug('signalling-server')
 log.error = debug('signalling-server:error')
 
-module.exports = {
+const conf = {
   log: log,
   hapi: {
     port: process.env.PORT || 13579,
@@ -17,5 +17,12 @@ module.exports = {
       }
     }
   },
-  refreshPeerListIntervalMS: 10000
+  refreshPeerListIntervalMS: 10000,
+  // this would reset the de-dup list for each peer
+  // so after X amount of seconds the peer will receive
+  // a all the peers again. It should be considerably
+  // larger that refreshPeerListIntervalMS
+  clearPeersSentListInterval: 1000 * 60 * 60 * 5 // clean every 5 mins
 }
+
+module.exports = conf
