a2bafe666eb0b5f5342f0d3fec078465c9ef4bef
guysv <sviryguy@gmail.com>
2019-05-17 19:23:07 +0300

    Update multiaddr format tests
    Old webrtc-star had the star server encoded into the multiaddr
    Because the rendezvous point is optional now (actually there is no
    handling of a explicit server) the format tests needed a refresh

--- test/transport/filter.js
+++ test/transport/filter.js
@@ -13,27 +13,19 @@ module.exports = (create) => {
       const ws = await create('a')
 
       const maArr = [
-        multiaddr('/ip4/127.0.0.1/tcp/9090/ws/p2p-webrtc-star/ipfs/QmcgpsyWgH8Y8ajJz1Cu72KnS5uo2Aa2LpzU7kinSoooo1'),
-        multiaddr('/ip4/127.0.0.1/tcp/9090/ws/p2p-webrtc-star'),
-        multiaddr('/dnsaddr/libp2p.io/ws/p2p-webrtc-star/ipfs/QmcgpsyWgH8Y8ajJz1Cu72KnS5uo2Aa2LpzU7kinSoooo1'),
-        multiaddr('/dnsaddr/signal.libp2p.io/ws/p2p-webrtc-star/ipfs/QmcgpsyWgH8Y8ajJz1Cu72KnS5uo2Aa2LpzU7kinSoooo1'),
-        multiaddr('/dnsaddr/signal.libp2p.io/wss/p2p-webrtc-star/ipfs/QmcgpsyWgH8Y8ajJz1Cu72KnS5uo2Aa2LpzU7kinSoooo1'),
-        multiaddr('/ip4/127.0.0.1/tcp/9090/ws/p2p-webrtc-star/ipfs/QmcgpsyWgH8Y8ajJz1Cu72KnS5uo2Aa2LpzU7kinSoooo2'),
-        multiaddr('/ip4/127.0.0.1/tcp/9090/ws/p2p-webrtc-star/ipfs/QmcgpsyWgH8Y8ajJz1Cu72KnS5uo2Aa2LpzU7kinSoooo3'),
-        multiaddr('/ip4/127.0.0.1/tcp/9090/ws/p2p-webrtc-star/ipfs/QmcgpsyWgH8Y8ajJz1Cu72KnS5uo2Aa2LpzU7kinSoooo4'),
-        multiaddr('/ip4/127.0.0.1/tcp/9090/ws/ipfs/QmcgpsyWgH8Y8ajJz1Cu72KnS5uo2Aa2LpzU7kinSoooo4'),
-        multiaddr('/ip4/127.0.0.1/tcp/9090/p2p-webrtc-star/ipfs/QmcgpsyWgH8Y8ajJz1Cu72KnS5uo2Aa2LpzU7kinSoooo4'),
-        multiaddr('/ip4/127.0.0.1/tcp/9090/p2p-webrtc-star/ipfs/QmcgpsyWgH8Y8ajJz1Cu72KnS5uo2Aa2LpzU7kinSoooo4' +
+        multiaddr('/p2p-webrtc-star/ipfs/QmcgpsyWgH8Y8ajJz1Cu72KnS5uo2Aa2LpzU7kinSoooo1'),
+        multiaddr('/p2p-webrtc-star'),
+        multiaddr('/p2p-webrtc-star/ipfs/QmcgpsyWgH8Y8ajJz1Cu72KnS5uo2Aa2LpzU7kinSoooo4' +
           '/p2p-circuit/ipfs/QmcgpsyWgH8Y8ajJz1Cu72KnS5uo2Aa2LpzU7kinSoooo1')
       ]
 
       const filtered = ws.filter(maArr)
-      expect(filtered.length).to.equal(7)
+      expect(filtered.length).to.equal(3)
     })
 
     it('filter a single addr for this transport', async () => {
       const ws = await create('a')
-      const ma = multiaddr('/ip4/127.0.0.1/tcp/9090/ws/p2p-webrtc-star/ipfs/QmcgpsyWgH8Y8ajJz1Cu72KnS5uo2Aa2LpzU7kinSoooo1')
+      const ma = multiaddr('/p2p-webrtc-star/ipfs/QmcgpsyWgH8Y8ajJz1Cu72KnS5uo2Aa2LpzU7kinSoooo1')
 
       const filtered = ws.filter(ma)
       expect(filtered.length).to.equal(1)
