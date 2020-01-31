14086b96f464f68eb83ed6539266a1bece4ea013
Jacob Heun <jacobheun@gmail.com>
2020-01-31 17:15:15 +0100

    fix: use menoetius to handle metrics (#197)

--- src/sig-server/bin.js
+++ src/sig-server/bin.js
@@ -10,8 +10,7 @@ const argv = require('minimist')(process.argv.slice(2))
   const server = await signalling.start({
     port: argv.port || argv.p || process.env.PORT || 9090,
     host: argv.host || argv.h || process.env.HOST || '0.0.0.0',
-    // Needs: https://github.com/libp2p/js-libp2p-webrtc-star/issues/174'
-    metrics: false // !(argv.disableMetrics || process.env.DISABLE_METRICS)
+    metrics: !(argv.disableMetrics || process.env.DISABLE_METRICS)
   })
 
   console.log('Listening on:', server.info.uri)
