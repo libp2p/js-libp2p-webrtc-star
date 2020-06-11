9ecedb415984843e774ca5b8658fa5c28ef8a80d
Vasco Santos <vasco.santos@moxy.studio>
2020-06-11 10:10:01 +0200

    fix: do not signal if channel destroyed

--- src/index.js
+++ src/index.js
@@ -175,7 +175,7 @@ class WebRTCStar {
           reject(errcode(offer.err instanceof Error ? offer.err : new Error(offer.err), 'ERR_SIGNALLING_FAILED'))
         }
 
-        if (offer.intentId !== intentId || !offer.answer) {
+        if (offer.intentId !== intentId || !offer.answer || channel.destroyed) {
           return
         }
 
