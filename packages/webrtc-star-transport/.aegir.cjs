4cdc7b2dbce272cd1ee3b8d1180ce3fdd5e75254
Alex Potsides <alex@achingbrain.net>
2022-03-19 11:42:54 +0000

    chore: enable node testing (#414)
    Registers shutdown hook to work around double-free that causes segfaults in recent node versions.

--- packages/webrtc-star-transport/.aegir.cjs
+++ packages/webrtc-star-transport/.aegir.cjs
@@ -1,5 +1,9 @@
 'use strict'
 
+// TODO: Temporary fix per wrtc issue
+// https://github.com/node-webrtc/node-webrtc/issues/636#issuecomment-774171409
+process.on('beforeExit', (code) => process.exit(code))
+
 let firstRun = true
 
 /** @type {import('aegir').PartialOptions} */
